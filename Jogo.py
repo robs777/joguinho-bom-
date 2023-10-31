5f22838d2955d2e1d2c9ca5641a4f252461723e8
rob <robson_appsantos@Hotmail.com>
2023-10-31 11:38:18 -0300

    primeiraF

--- Jogo.py
+++ Jogo.py
@@ -1,28 +1,64 @@
-#   1° obter_escolha_usuario 
-
-#   2° gerar_escolha_computador 
-
 import random
 
-Mylist = ["Pedra", "Papel", "Tesoura"]
 
-#função
-def gerar_escolha_computador(A,B,C):
+def obter_escolha_usuario():
+    escolha_usuario = input("Escolha: pedra (1), papel(2) ou tesoura(3)")
+    if escolha_usuario == "1":
+        return "pedra"
+    elif escolha_usuario == "2":
+        return "papel"
+    elif escolha_usuario == "3":
+        return "tesoura"
+    else:
+        print ("Escolha inválida. Por favor digite 1, 2 ou 3")
+        return obter_escolha_usuario()
+
+
+
+
+def gerar_escolha_computador(escolha_usuario):
+    pass
     
-    AS = "Pedra"
-    BS = "Papel"
-    CS = "Tesoura"
-    computador = random.choices(Mylist(A, B, C))
 
 
-###########################################
-#código principal
 
-AS = "Pedra"
-BS = "Papel"
-CS = "Tesoura"
-print(gerar_escolha_computador(AS, BS, CS))
 
-#   3° determinar_vencedor 
 
-#   4° jogar_novamente 
+# def determinar_vencedor():
+
+
+
+
+# def jogar_novamente():
+
+
+
+
+
+
+def main():
+    
+    while True:
+        print("-"*20)
+        print("Jokenpo")
+        print("-"*20)
+        print("Vamos jogar?")
+        us_digit= input("digite sim ou não")
+
+        if  us_digit == "sim":
+            abcd = obter_escolha_usuario()
+            print(f"abcd == {abcd}")
+            gerar_escolha_computador(abcd)
+        
+        elif us_digit == "não":
+            print("que pena, nos vemos em breve")
+            break
+        
+        else:
+            print(f"opção {us_digit} invalida. Digite apenas sim ou não")
+
+
+
+
+if __name__=="__main__":
+    main()
